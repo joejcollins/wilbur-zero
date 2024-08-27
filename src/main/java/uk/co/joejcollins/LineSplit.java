01c418142d1ec933f2fcf33c7db95c2ce19fbdec
Joe J Collins <joejcollins@users.noreply.github.com>
2024-08-27 17:02:57 +0000

    Working compile

--- src/main/java/uk/co/joejcollins/LineSplit.java
+++ src/main/java/uk/co/joejcollins/LineSplit.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package uk.co.joejcollins;
 
 import org.apache.kafka.common.serialization.Serdes;
@@ -28,10 +12,11 @@ import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * In this example, we implement a simple LineSplit program using the high-level Streams DSL
- * that reads from a source topic "streams-plaintext-input", where the values of messages represent lines of text;
- * the code split each text line in string into words and then write back into a sink topic "streams-linesplit-output" where
- * each record represents a single word.
+ * In this example, we implement a simple LineSplit program using the high-level
+ * Streams DSL that reads from a source topic "streams-plaintext-input", where
+ * the values of messages represent lines of text; the code split each text line
+ * in string into words and then write back into a sink topic
+ * "streams-linesplit-output" where each record represents a single word.
  */
 public class LineSplit {
 
@@ -45,8 +30,8 @@ public class LineSplit {
         final StreamsBuilder builder = new StreamsBuilder();
 
         builder.<String, String>stream("streams-plaintext-input")
-            .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
-            .to("streams-linesplit-output");
+                .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .to("streams-linesplit-output");
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
